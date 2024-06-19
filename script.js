9646601e5cbe0d4f1bece78ee3ff258c4845fe51
CamilaJakusik <camilajakusik@gmail.com>
2024-06-19 18:45:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.getElementById('add-task').addEventListener('click', function() {
         taskInput.focus();
     }
 });
+
 document.getElementById('new-task').addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         document.getElementById('add-task').click();
@@ -17,9 +18,11 @@ function addTask(taskText) {
     var taskList = document.getElementById('task-list');
 
     var li = document.createElement('li');
+    li.className = 'box is-flex is-align-items-center is-justify-content-space-between'; // Estilo compacto Bulma
     li.textContent = taskText;
 
     var deleteButton = document.createElement('button');
+    deleteButton.className = 'button is-danger is-small'; // Estilo Bulma para el botón de eliminar
     deleteButton.textContent = 'Eliminar';
     deleteButton.addEventListener('click', function() {
         taskList.removeChild(li);
